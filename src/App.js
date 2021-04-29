0ccaa8d6faf4cade625ffccbf207405a0e5737f2
mirzajiji31 <mirza.jijieshvili.1@btu.edu.ge>
2021-04-29 20:28:08 +0400

    shualeduri v1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
 import Welcome from "./components/welcome";
 import HomePage from "./pages/home";
-
-import "./App.css";
 import TodoHome from "./pages/todo";
+import Counter from "./pages/counter";
+
+import Navigation from "./components/navigation";
+import "./App.css";
+
+import { useState } from "react";
+import { object } from "prop-types";
 
 function App() {
+  const [page, setPage] = useState({
+    homePage: true,
+    todoHome: false,
+    counter: false,
+  });
+
+  const onPageChange = (pageKey) => {
+    const updatePages = { ...page };
+    // Object.keys(page).forEach((key) => {
+    //   if (key === pageKey) {
+    //     updatePages[pageKey] = true;
+    //   } else {
+    //     updatePages[key] = false;
+    //   }
+    // });
+
+    for (let key of Object.keys(updatePages)) {
+      if (key === pageKey) {
+        updatePages[pageKey] = true;
+      } else {
+        updatePages[key] = false;
+      }
+    }
+
+    setPage(updatePages);
+  };
+
   return (
     <div className="container">
       {/* <Welcome /> */}
-      {/* <HomePage /> */}
+      <Navigation onPageChange={onPageChange} pages={page} />
+      <hr />
 
-      <TodoHome />
+      {page.homePage && <HomePage />}
+      {page.todoHome && <TodoHome />}
+      {page.counter && <Counter />}
     </div>
   );
 }
